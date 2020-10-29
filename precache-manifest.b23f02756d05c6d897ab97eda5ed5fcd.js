b5e3fc139e26eee5481c5ea01471b320d609eb5d
phalas <piothalas@gmail.com>
2020-10-30 00:56:57 +0100

    Updates

---
+++ precache-manifest.b23f02756d05c6d897ab97eda5ed5fcd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05185e4c45361c84c9a5ec57e269c67",
+    "url": "/dnccollect-page/index.html"
+  },
+  {
+    "revision": "4784a740963c4a36a1fc",
+    "url": "/dnccollect-page/static/css/main.3525bd11.chunk.css"
+  },
+  {
+    "revision": "b2aa7919bf506026d86d",
+    "url": "/dnccollect-page/static/js/2.72a447e3.chunk.js"
+  },
+  {
+    "revision": "d32618f5e405be531eaba7f5d1363c60",
+    "url": "/dnccollect-page/static/js/2.72a447e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4784a740963c4a36a1fc",
+    "url": "/dnccollect-page/static/js/main.385ec55d.chunk.js"
+  },
+  {
+    "revision": "1e0f9688a4487502952b",
+    "url": "/dnccollect-page/static/js/runtime-main.e972a03c.js"
+  },
+  {
+    "revision": "76d6045c7310e63730484c87029bbd63",
+    "url": "/dnccollect-page/static/media/mobile-yt-video.76d6045c.png"
+  },
+  {
+    "revision": "761e27d8b95b17b8ede0095bd263a324",
+    "url": "/dnccollect-page/static/media/search-mobile.761e27d8.png"
+  },
+  {
+    "revision": "be4acd8c18bcec3061a454d39d43b35e",
+    "url": "/dnccollect-page/static/media/video-list.be4acd8c.png"
+  }
+]);
