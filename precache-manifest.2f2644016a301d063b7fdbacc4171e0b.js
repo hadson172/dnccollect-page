822a646bfb330058bdef0b0212864c5ab1127247
phalas <piothalas@gmail.com>
2020-10-30 01:02:27 +0100

    Updates

---
+++ precache-manifest.2f2644016a301d063b7fdbacc4171e0b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc9f4e4ddafa99935f24900762c06b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f7d769e95093f39ce4f",
+    "url": "/static/css/main.3525bd11.chunk.css"
+  },
+  {
+    "revision": "5d0d1a31a13399f7d702",
+    "url": "/static/js/2.51d91d03.chunk.js"
+  },
+  {
+    "revision": "d32618f5e405be531eaba7f5d1363c60",
+    "url": "/static/js/2.51d91d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7d769e95093f39ce4f",
+    "url": "/static/js/main.0565a9ec.chunk.js"
+  },
+  {
+    "revision": "4e4a27b3d2ad2352e955",
+    "url": "/static/js/runtime-main.b57b22ce.js"
+  },
+  {
+    "revision": "76d6045c7310e63730484c87029bbd63",
+    "url": "/static/media/mobile-yt-video.76d6045c.png"
+  },
+  {
+    "revision": "761e27d8b95b17b8ede0095bd263a324",
+    "url": "/static/media/search-mobile.761e27d8.png"
+  },
+  {
+    "revision": "be4acd8c18bcec3061a454d39d43b35e",
+    "url": "/static/media/video-list.be4acd8c.png"
+  }
+]);
